bbd34e2f873708aa2266dab960aa84afcf92251e
Alexander E. Zarebski <36244258+aezarebski@users.noreply.github.com>
2020-10-31 13:07:08 +0000

    Aez code review (#3)
    * put filenames as variables and adjust documentation for readability
    
    * avoid hardcoding the number of areas
    
    * make sure that you can read the data back from file correctly
    
    * expose that the adjacency matrix is not symmetric

--- mobility-matrix-inla-graph.R
+++ mobility-matrix-inla-graph.R
@@ -1,10 +1,25 @@
 library(dplyr)
 library(INLA)
 
-##NOTE download files from dropbox folder called "spatial-matrix"##
+
+#' The threshold above which we consider to areas to be adjacent.
+thresh <- 175 #550 #225
+
+#' NOTE download files from dropbox folder called "spatial-matrix". The
+#' \code{OUTPUT_FILE} is where we write the final result to.
+
+MOBILITY_XLSX <- "mobility_mat17OCT.xlsx"
+PEDRO_NAMES_CSV <- "code_muni_names_pedro.csv"
+MUNI_NAMES_CSV <- "code_muni_names_sp.csv"
+
+OUTPUT_FILE <- "id_list_forinla.txt"
+
+stopifnot(file.exists(MOBILITY_XLSX))
+stopifnot(file.exists(PEDRO_NAMES_CSV))
+stopifnot(file.exists(MUNI_NAMES_CSV))
 
 #import mobility data for the month of March 2020
-mob<-readxl::read_excel("mobility_mat17OCT.xlsx") %>%
+mob<-readxl::read_excel(MOBILITY_XLSX) %>%
   select(-col) 
 colnames(mob)=c(1:nrow(mob))
 
@@ -14,10 +29,9 @@ mob_mat<- as.matrix(mob)
 #set within municipality trips to 0 
 diag(mob_mat) <- 0
 
-#set trip threshold to 175
-thresh <- 175 #550 #225
-
-#convert matrix to a binary matrix where 1=number of trips taken exceeds threshold and 0=number of trips below threshold
+#' convert matrix to a binary matrix where 1=number of trips taken exceeds
+#' threshold and 0=number of trips below threshold. The multiplication by 1 is
+#' there to force R to represent the logical values in the matrix as numbers.
 mat_bin<-mob_mat>thresh 
 mat_bin<-mat_bin*1
 
@@ -25,21 +39,25 @@ mat_bin<-mat_bin*1
 dimnames(mat_bin) <- NULL
 
 #import order of municipalities based on matrix
-muni_mat<-read.csv("code_muni_names_pedro.csv") %>%
+muni_mat<-read.csv(PEDRO_NAMES_CSV) %>%
   rename(idarea2=idarea)
 
-#import code_muni_sp.csv which serves as the reference list 
-muni_sp<-read.csv("code_muni_names_sp.csv") %>%
+#import code_muni_sp.csv which serves as the reference list
+muni_sp<-read.csv(MUNI_NAMES_CSV) %>%
   rename(idarea1=idarea)
 
 #create id dictionary that links id from code_muni_sp.csv to id/location in matrix
 dict<-left_join(muni_sp,muni_mat[,c("code_muni","idarea2")],by="code_muni")
 
-#create function that extracts the idarea of each matrix cell based on the definitive list of muni names
+#' create function that extracts the idarea of each matrix cell based on the
+#' definitive list of muni names
 idarea_extract<- function(mat_bin) {
 id_list <- vector(mode = "list", length = 1)
-#first entry must be the number of nodes in the graph
-id_list[1]<-645
+
+#' first entry must be the number of nodes in the graph, to avoid hardcoding
+#' this we read the number of rows in the matrix
+id_list[1]<- nrow(mat_bin)
+
 #determine id of reference list 
   for (i in dict$idarea1) {
     #find corresponding id of mobility matrix
@@ -65,7 +83,7 @@ return(id_list)
 id_list_forinla<-idarea_extract(mat_bin)
 
 #save to text file 
-save_file<-lapply(id_list_forinla, write, "id_list_forinla.txt", append=TRUE, ncolumns=1000)
+save_file<-lapply(id_list_forinla, write, OUTPUT_FILE, append=TRUE, ncolumns=1000)
 
 #convert list to vector only so it becomes an ascii file which is used as inlagraph input
 id_forinla<-unlist(id_list_forinla, use.names=FALSE)
@@ -79,6 +97,17 @@ inla.spy(g)
 
 #alternative way to plot graph
 image(inla.graph2matrix(g),xlab="",ylab="") 
+
+#' To be confident that the graph has been written to file correctly we can read
+#' it in from file and compare it to the result we already have. Because
+#' equality is not implemented for the \code{inla.graph} class we have to
+#' translate them to a matrix first.
+g2 <- inla.read.graph(OUTPUT_FILE)
+stopifnot(all(inla.graph2matrix(g) == inla.graph2matrix(g2)))
+
+#' We also want to know that the matrix is symmetric because this is a property
+#' of any adjacency matrix.
+stopifnot(all(inla.graph2matrix(g2) == t(inla.graph2matrix(g2))))
 
 #save matrix as an inla graph
 saveRDS(g,"mobility_spatial_matrix_spstate_175.rds")
